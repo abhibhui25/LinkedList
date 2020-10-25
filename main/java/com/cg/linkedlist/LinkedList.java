aa8a7a649662de1715c086f5f59fcf15fbc96d04
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-25 21:02:25 +0530

    welcome

---
+++ main/java/com/cg/linkedlist/LinkedList.java
@@ -0,0 +1,53 @@
+package com.cg.linkedlist;
+
+public class LinkedList {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node temp= null;
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+            temp = newNode;
+        }
+        else {
+            temp.next = newNode;
+            temp = newNode;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        if (head == null) {
+            System.out.println("The list is empty");
+            return;
+        }
+        System.out.println("The List contains: ");
+        while (current != null)
+        {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList List = new LinkedList();
+        List.addNode(70);
+        List.addNode(30);
+        List.addNode(56);
+        List.display();
+    }
+}
